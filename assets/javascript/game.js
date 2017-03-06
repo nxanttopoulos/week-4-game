4950630b9815bc57115c6bfb8cc871593c851c87
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-03-06 16:45:55 -0500

    Fixed a few html mistakes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 	function randomStartingNum(result) {
 	    randomNum = Math.floor(Math.random() * 102)+19;
-	    $("#randomNumber").html("<h2>"+randomNum+"</h2>");
+	    $("#randomNumber").html("<h2 id='text2'>"+randomNum+"</h2>");
 	    playerNum = 0;
 	}
 
@@ -16,15 +16,15 @@ $(document).ready(function() {
 	function didYouWin() {
 	if (playerNum === randomNum) {
 		    wins++;
-		    $("#winsLosses").html("<h2 id='text'>Wins:"+ wins +"<br>Losses:"+ losses +"</h2>");       
+		    $("#winsLosses").html("<h2 id='text'>You Win!!<br>Wins:"+ wins +"<br>Losses:"+ losses +"</h2>");       
 			playerNum = 0;
-			$("#playerNumber").html("<h2>" + playerNum +"</h2>");
+			$("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 			randomStartingNum();
 		} else if (playerNum > randomNum) {
 		    losses++;
-		    $("#winsLosses").html("<h2 id='text'>Wins:"+ wins +"<br>Losses:"+ losses +"</h2>");
+		    $("#winsLosses").html("<h2 id='text'>You Lost!!<br>Wins:"+ wins +"<br>Losses:"+ losses +"</h2>");
 		    playerNum = 0;
-		    $("#playerNumber").html("<h2>" + playerNum +"</h2>");
+		    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 		    randomStartingNum();
 		}
 	}
@@ -32,40 +32,40 @@ $(document).ready(function() {
 	$("#crystalOne").on("click", function() {
 	    if (playerNum === 0) {
 	    playerNum = 12;
-	    $("#playerNumber").html("<h2>" + playerNum +"</h2>");
+	    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 		} else 
 		playerNum = playerNum+12;
-	    $("#playerNumber").html("<h2>" + playerNum +"</h2>"); 
+	    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>"); 
 	    didYouWin();
 	});
 
 	$("#crystalTwo").on("click", function() {
 	    if (playerNum === 0) {
 	    playerNum = 6;
-	    $("#playerNumber").html("<h2>" + playerNum +"</h2>");
+	    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 		} else 
 		playerNum = playerNum+6;
-	    $("#playerNumber").html("<h2>" + playerNum +"</h2>");
+	    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 		didYouWin();
 	});
 
 	$("#crystalThree").on("click", function() {
 	    if (playerNum === 0) {
 	    playerNum = 10;
-	    $("#playerNumber").html("<h2>" + playerNum +"</h2>");
+	    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 		} else 
 		playerNum = playerNum+10;
-	    $("#playerNumber").html("<h2>" + playerNum +"</h2>");
+	    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 		didYouWin();
 	});
 
 	$("#crystalFour").on("click", function() {
 	    if (playerNum === 0) {
 	    playerNum = 3;
-	    $("#playerNumber").html("<h2>" + playerNum +"</h2>");
+	    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 		} else 
 		playerNum = playerNum+3;
-	    $("#playerNumber").html("<h2>" + playerNum +"</h2>");
+	    $("#playerNumber").html("<h2 id='text2'>" + playerNum +"</h2>");
 		didYouWin();
 	});
 });
